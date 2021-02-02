911d6916ff480240b3472077bd4ee10cfbcc4d5e
Aryan Agarwal <aryankush025@gmail.com>
2021-02-02 23:12:07 +0530

    SSL added to env

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,6 +3,9 @@ const ormConfigs = {
   type: process.env.ORM_CONFIG_TYPE,
   synchronize: JSON.parse(process.env.ORM_CONFIG_SYNCHRONIZE),
   logging: JSON.parse(process.env.ORM_CONFIG_LOGGING),
+  extra: {
+    ssl: JSON.parse(process.env.ORM_CONFIG_SSL),
+  },
 };
 
 module.exports = {
@@ -12,8 +15,5 @@ module.exports = {
   cli: {
     entitiesDir: 'src/database/entity',
     migrationsDir: 'src/database/migration',
-  },
-  extra: {
-    ssl: true,
   },
 };
